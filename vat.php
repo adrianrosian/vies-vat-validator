767e95ca38b57c83dc018d7c2b5534cdfe264933
adrianrosian <adrian.rosian@gmail.com>
2014-03-19 23:13:10 +0200

    Quick remix to function in command line

--- vat.php
+++ vat.php
@@ -1,3 +1,4 @@
+#!/usr/bin/php
 <?php
 /**
  * LICENSE
@@ -19,74 +20,96 @@
  * limitations under the License.
 */
 
-class VatValidator
-{
-	public $response;
-	protected $soap;
-	
-	// WSDL VIES Url Service.
-	protected static $url_vies = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';
-	
-	// Valid european countries ISO codes.
-	protected static $european_countries = array(
-		'AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FI', 'FR', 'GB', 'GR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 'PT', 'RO', 'SE', 'SI', 'SK'
-	);
-	
-	public function __construct ()
-	{
-		$this->soap = new SoapClient( self::$url_vies );
-	}
+if ($argc != 3 || in_array($argv[1], array('--help', '-help', '-h', '-?'))) {
+    ?>
 
-	/**
-	 * Check if it's a valid vat number.
-	 */
-	public function checkVat ( $country, $number )
-	{
-		$response = array( 'is_valid' => false );
-		$vat = $this->prepareVat( $country, $number );
-		if ($vat)
-		{
-			$this->response = array( 'is_valid' =>$this->soap->checkVat( $vat )->valid );
-		}
-		return json_encode( $this->response );
-	}
+    This script allows you to check the validity of an EU VAT code against the VIES system
 
-	/**
-	* Checks that there are all needed params ( Code Country and number );
-	*/
-	protected function prepareVat( $country, $number )
-	{
-	try
-		{
-			if ( empty( $country ) || empty( $number ) )
-			{
-				throw new Exception( "Both 'country' and 'number' params are mandatory" );
-			}
-			
-			if ( !in_array( $country, self::$european_countries ) )
-			{
-				throw new Exception( "Invalid country" );
-			}
-			
-			$vat = array(
-				'vatNumber'		=> $number,
-				'countryCode'	=> $country,
-				
-			);
-			return $vat;
-		}
+    Usage:
+    <?php echo $argv[0]; ?> <country_code> <VAT_number>
 
-		catch( Exception $e )
-		{
-			$this->response = array( 'error_message' => $e->getMessage() );
-			return false;
-		}
-	}
+    --help, -h, -help, -? Prints out this help
+    <country_code> two letter country code (eg. RO, GB).
+    <VAT_number> the VAT number of the company
+
+    Example:
+    php vies.php GB 786876768
+
+    <?php
+} else {
+    class VatValidator
+    {
+        public $response;
+        protected $soap;
+
+        // WSDL VIES Url Service.
+        protected static $url_vies 
+            = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';
+
+        // Valid european countries ISO codes.
+        protected static $european_countries = array(
+            'AT', 'BE', 'BG', 'CY', 'CZ', 'DE', 'DK', 'EE', 'ES', 'FI', 'FR', 
+            'GB', 'GR', 'HU', 'IE', 'IT', 'LT', 'LU', 'LV', 'MT', 'NL', 'PL', 
+            'PT', 'RO', 'SE', 'SI', 'SK'
+            );
+
+        public function __construct()
+        {
+            $this->soap = new SoapClient(self::$url_vies);
+        }
+
+        /**
+        * Check if it's a valid vat number.
+        */
+        public function checkVat($country, $number)
+        {
+            $response = array( 'is_valid' => false );
+            $vat = $this->prepareVat($country, $number);
+            if ($vat) {
+                $this->response = array(
+                    'is_valid' => $this->soap->checkVat($vat)->valid
+                );
+            }
+
+            return json_encode($this->response);
+        }
+
+        /**
+        * Checks that there are all needed params ( Code Country and number );
+        */
+        protected function prepareVat($country, $number)
+        {
+            try {
+                if ( empty( $country ) || empty( $number ) ) {
+                    throw new Exception("Both 'country' and 'number' params are mandatory");
+                }
+
+                if ( !in_array($country, self::$european_countries)) {
+                    throw new Exception("Invalid country");
+                }
+
+                $vat = array(
+                    'vatNumber'	=> $number,
+                    'countryCode'	=> $country,
+
+                    );
+
+                return $vat;
+            } catch ( Exception $e ) {
+                $this->response = array( 'error_message' => $e->getMessage() );
+
+                return false;
+            }
+        }
+    }
+
+    // API Call
+    $vies = new VatValidator();
+    $vies->checkVat(strtoupper($argv[1]), $argv[2]);
+    if (array_key_exists('error_response', $vies->response))
+        echo $vies->response['error_response'];
+    else
+        echo $vies->response['is_valid'] ? 'Code is valid' : 'Code is not valid';
+
+    echo "\n";
 }
-
-// API Call
-$vies = new VatValidator();
-$vies->checkVat( strtoupper( $_GET['country'] ), $_GET['number']);
-$response = json_encode( $vies->response);
-echo $response;
-?>
